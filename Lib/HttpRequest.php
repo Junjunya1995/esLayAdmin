551a6742d35ecd6688cf0fc47da8223b48b546c7
junjunya <1039845982@qq.com>
2018-06-16 09:44:45 +0800

    完善 新增 仿TP5 param方法

--- Lib/HttpRequest.php
+++ Lib/HttpRequest.php
@@ -11,26 +11,49 @@ namespace Lib;
 
 use EasySwoole\Core\Http\Request;
 
+/**
+ * Class HttpRequest
+ * @package Lib
+ */
 class HttpRequest
 {
+
     private  $request = null;
+
     public function __construct(Request $request)
     {
         $this->request = $request;
     }
 
+    /**
+     * 获取请求方式 GET POST
+     * @author wzj
+     * @return mixed
+     */
     public function getMethod() {
         return $this->request->getServerParams()['request_method'];
     }
 
+    /**
+     * 判断是否是POST 请求
+     * @return bool
+     */
     public function isPost() {
-        return $this->getMethod() === 'POST' ? true : false;
+        return $this->getMethod() === 'POST';
     }
 
+    /**
+     * 判断是否是 GET 请求
+     * @return bool
+     */
     public function isGet() {
-        return $this->getMethod() === 'GET' ? true : false;
+        return $this->getMethod() === 'GET';
     }
 
+    /**
+     * 判断是否是Ajax 请求
+     * @return bool
+     */
     public function isAjax() {
         $ajax = $this->request->getHeader('x-requested-with');
         if(empty($ajax)) {
@@ -39,8 +62,51 @@ class HttpRequest
         return $ajax[0] === 'XMLHttpRequest';
     }
 
+    /**
+     * 获取 客户端IP
+     * @author wzj
+     * @return string
+     */
     public function getIp() {
         $ip = $this->request->getHeader('x-real-ip');
         return  count($ip)  ? $ip[0] : '0.0.0.0';
+    }
+
+    /**
+     * 获取当前请求的参数
+     * @param string $name
+     * @param null   $default
+     * @param string $filter
+     * @author wzj
+     * @return array|null
+     */
+    public function param($name = '', $default = null, $filter = '') {
+        //合并请求参数
+        $param = array_merge($this->request->getQueryParams(), $this->request->getParsedBody());
+
+        if (!empty($name)) {
+            if (!empty($filter)) {
+                $param[$name] = $filter($param[$name]);
+            }
+            return $param[$name] ?? $default;
+        }
+
+        foreach ($param as $k => $v) {
+            if (!empty($filter)) {
+                $v = $filter($v);
+            }
+            $param[$k] = $v ?? $default;
+        }
+        return $param;
+    }
+
+    /**
+     * 继承 EasySwoole\Core\Http\Request的所有方法
+     * @param $method
+     * @param $args
+     * @return mixed
+     */
+    public function __call($method, $args) {
+        return call_user_func_array([$this->request, $method], $args);
     }
 }
